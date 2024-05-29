979a0a6cd05248ac89a50c0c3749ece892eb123e
IaraMoscovich <imosco2007@gmail.com>
2024-05-29 14:29:15 -0300

    Cambie todo

--- src/components/upload-image.tsx
+++ src/components/upload-image.tsx
@@ -1,13 +1,10 @@
 "use client"
 
-import { createBrowserClient } from '@supabase/ssr';
+import { createBrowser } from './supabase_client_client';
 import { ChangeEvent } from 'react';
 
 export default function Profile() {
-    const supabase = createBrowserClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL!,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    )
+    const supabase = createBrowser();
   
     // Handle file upload event
     const uploadFile = async (event: ChangeEvent<HTMLInputElement>) => {
