c7704801935655acf5d8f2240e07b5563be89b4f
IaraMoscovich <imosco2007@gmail.com>
2024-06-24 15:51:18 -0300

    conexion ia con back

---
+++ src/app/IA_connection/IA.ts
@@ -0,0 +1,29 @@
+/*const axios = require('axios');
+
+axios.get('https://fastapi-example-endl.onrender.com/', {  
+    firstName: 'Fred',
+    lastName: 'Flintstone'
+  })
+  .then(function (response:unknown) {
+    console.log(response);
+  })
+  .catch(function (error:unknown) {
+    console.log(error);
+  });*/
+
+  /*const xhr = new XMLHttpRequest();
+xhr.open("POST", "https://jsonplaceholder.typicode.com/posts");
+xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+const body = JSON.stringify({
+  title: "Hello World",
+  body: "My POST request",
+  userId: 900,
+});
+xhr.onload = () => {
+  if (xhr.readyState == 4 && xhr.status == 201) {
+    console.log(JSON.parse(xhr.responseText));
+  } else {
+    console.log(`Error: ${xhr.status}`);
+  }
+};
+xhr.send(body);*/
