77b3967fa6e236c72a5c7de6cc4f595542d30dce
IaraMoscovich <imosco2007@gmail.com>
2024-07-02 08:04:46 -0300

    termine la conexion de la ia con el colab

--- src/app/IA_connection/IA.ts
+++ src/app/IA_connection/IA.ts
@@ -1,18 +1,18 @@
 import { error } from "console";
 
 const axios = require('axios');
-const url = 'http://localhost:3000/'; //chequear si esta bien
+const url = ''; //chequear si esta bien
 
-axios.get('https://fastapi-example-endl.onrender.com/', {  
-    firstName: 'Fred',
-    lastName: 'Flintstone'
-  })
-  .then(function (response:unknown) {
-    console.log(response);
-  })
-  .catch(function (error:unknown) {
-    console.log(error);
-  });
+export async function postReq(body:string) {
+  try{
+    const response = await axios.post("https://fastapi-example-endl.onrender.com/upload-image", body).then((res:unknown) => {
+      return response.data;
+    })
+  } catch(error:unknown){
+    console.log(error)
+    return 'error';
+  }
+}
 
   // Declaración de CancerMamaData (sustituye los datos según corresponda)
 const CancerMamaData = {
@@ -20,27 +20,6 @@ const CancerMamaData = {
   testDate: new Date(),
   // Otros datos relevantes para la espirometría
 };
-
-try {
-  const obstruction = async() => await axios.post("direccion del server", CancerMamaData)
-      .then((res:any) => {
-          if (res.data || res.data.result == undefined) {
-              throw new Error("Respuesta de servidor incorrecta para obstrucción");
-          }
-          return res.data.number
-      })
-      .catch((err: unknown) => {
-          throw new Error("Error al analizar la obstrucción: " + err);
-      });
-
-
-  // Usar los resultados de obstruction y restriction aquí
-  console.log("Obstrucción:", obstruction);
-
-} catch (error) {
-  console.error("Se produjo un error durante la solicitud:", error);
-  // Manejar el error de manera apropiada, por ejemplo, enviarlo a un servicio de logs
-}
 
  
   
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import { createServer } from '@/components/supabase_server_client';
 import Profile from '@/components/upload-image';
 
+import { postReq } from '@/app/IA_connection/IA'
+
 export default async function Home() {
+
+  const a = await postReq("hola")
+  console.log(JSON.stringify(a))
 
   const supabase = createServer();
   const { data, error } = await supabase.from("medico_users").select("*")
