1102509f162441df3e5be907f51ebefb53b00fcf
frrannn <110538082+frrannn@users.noreply.github.com>
2024-08-08 15:22:19 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
+export default defineConfig({
+ image: {
+   service: passthroughImageService(),
+ },
+});
+
 
 // https://astro.build/config
 export default defineConfig({
